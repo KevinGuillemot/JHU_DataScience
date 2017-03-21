adc3b59f5f9801bd1e379ed11edd5f82bf5ae60e
Kevin Guillemot <guillemot.k@gmail.com>
2017-03-21 00:09:32 -0400

    Finished quizz

--- 3 Data cleaning/C3W4.R
+++ 3 Data cleaning/C3W4.R
@@ -29,9 +29,26 @@ str(housing)
 strsplit(names(housing),"wgtp")[123]
 
 #2
-gdp<-read.csv("getdata_data_GDP.csv")
+gdp<-read.csv("getdata%2Fdata%2FGDP.csv")
 cleangdp<-gdp
 cleangdp$X.3<-as.numeric(gsub(",","",as.character(cleangdp$X.3)))
 cleangdp$Gross.domestic.product.2012<-as.numeric(as.character(cleangdp$Gross.domestic.product.2012))
 mean(cleangdp$X.3[complete.cases(cleangdp$X.3,cleangdp$Gross.domestic.product.2012)])
+
+
+#4
+gdp<-read.csv("getdata%2Fdata%2FGDP.csv")
+cleangdp<-gdp
+cleangdp$Gross.domestic.product.2012<-as.numeric(as.character(cleangdp$Gross.domestic.product.2012))
+cleangdp<-cleangdp[complete.cases(cleangdp$Gross.domestic.product.2012),]
+edu<-read.csv("getdata%2Fdata%2FEDSTATS_Country.csv")
+m<-merge(cleangdp,edu,by.x = "X",by.y = "CountryCode",all=FALSE)
+length(grep("*Fiscal year end: June*",m$Special.Notes))
+
+#5
+library(quantmod)
+amzn = getSymbols("AMZN",auto.assign=FALSE)
+sampleTimes = index(amzn)
+sum(format(sampleTimes,"%Y")=="2012")
+sum(format(sampleTimes,"%Y%A")=="2012Monday")
 
